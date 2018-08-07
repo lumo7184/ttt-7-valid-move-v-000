02dbd68334a199e537b8b678726053b3a48cfc81
lucas montoni <lmontoni@outlook.com>
2018-08-07 18:59:07 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,7 +7,7 @@ end
 # re-define your #position_taken? method here, so that you can use it in the
 
 def position_taken?(board,position)
-  return false if [" ", "", nil].include?(board[position])
-  return true if ["X", "O"].include?(board[position])
+  return false if [position] != ""
+  return true if [position] != ""
   raise "#{board[position]} is not a valid move"
 end
